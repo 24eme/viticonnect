e92137d5b49ddf57af6c4ff829996934b7c5725a
Tangui Morlier <tangui@tangui.eu.org>
2022-03-14 12:56:50 +0100

    Bootstrap 5

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -1,15 +1,28 @@
-<html>
+<?php
+ 
+    $title = (isset($title) && $title) ? $title : "Viticonnect" 
+ 
+?><!doctype html>
+<html lang="fr">
 <head>
-<title><?php echo (isset($title) && $title) ? $title : "Viticonnect" ; ?></title>
-<link href="css/bootstrap.min.css?v=5.1.3" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap CSS -->
+<link href="/css/bootstrap.min.css?v=5.1.3" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- title -->
+<title><?php echo $title; ?></title>
 </head>
-<body>
-<h1>Viticonnect</h1>
+<body class="text-center">
 
-<div id="pagecontent">
+<header class="mb-4">
+<h1 class="h3 mb-3 fw-normal"><?php echo $title; ?></h1>
+</header>
+
+<main class="container">
 <?php include($template); ?>
-</div>
+</main>
 
-</body>
 <script src="/js/bootstrap.bundle.min.js?v=5.1.3" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
 </html>
