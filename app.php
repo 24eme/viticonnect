053dd4999c849f82fa65d9406d7e9111701796a8
Tangui Morlier <tangui@tangui.eu.org>
2022-04-11 14:42:59 +0200

    Reorganisation des routes et de la configuration

--- app.php
+++ app.php
@@ -2,26 +2,18 @@
 
 $f3 = require(__DIR__.'/vendor/fatfree/lib/base.php');
 
-if(getenv("DEBUG")) {
-    $f3->set('DEBUG', getenv("DEBUG"));
-}
-$f3->set('ROOT', __DIR__);
-$f3->set('UI', $f3->get('ROOT')."/templates/");
 
-$port = $f3->get('PORT');
-$f3->set('urlbase', $f3->get('SCHEME').'://'.$_SERVER['SERVER_NAME'].(!in_array($port,[80,443])?(':'.$port):'').$f3->get('BASE'));
-
-require_once('config/services.config.php');
-$f3->set('services', $services);
-
-$f3->route('GET /', function($f3) {
+class VitiConnect {
+    
+    public function home(Base $f3) {
         $service = '';
         if ($f3->exists('GET.service')) {
             $service = '?service='.$f3->get('GET.service');
         }
         $f3->reroute('/cas/login'.$service);
-    });
-$f3->route('GET /cas/login', function($f3) {
+    }
+    
+    public function cas_login_get(Base $f3) {
         $cases = $f3->get('services');
         if ($f3->exists('GET.service')) {
             $service = $f3->get('GET.service');
@@ -49,18 +41,18 @@ $f3->route('GET /cas/login', function($f3) {
         $f3->set('cases', $cases);
         $f3->set('template', 'login.html.php');
         echo View::instance()->render('layout.html.php');
-});
-
-$f3->route('POST /cas/login', function($f3) {
-    $cases = $f3->get('services');
-    $service = $f3->get('POST.service');
-    $key = $f3->get('POST.cas_choice');
-    $callback = $f3->get('urlbase')."/callback/".base64_encode($service)."/%servicename%";
-    $url = str_replace('%service%', str_replace('%servicename%', $key, $callback), $cases[$key]['cas_login']);
-    return $f3->reroute($url);
-});
-
-$f3->route('GET /callback/@callback/@origin', function($f3) {
+    }
+    
+    public function cas_login_post(Base $f3) {
+        $cases = $f3->get('services');
+        $service = $f3->get('POST.service');
+        $key = $f3->get('POST.cas_choice');
+        $callback = $f3->get('urlbase')."/callback/".base64_encode($service)."/%servicename%";
+        $url = str_replace('%service%', str_replace('%servicename%', $key, $callback), $cases[$key]['cas_login']);
+        return $f3->reroute($url);
+    }
+    
+    public function callback(Base $f3) {
         $service = base64_decode($f3->get('PARAMS.callback'));
         $ticket = $f3->get('GET.ticket').'%origin:'.$f3->get('PARAMS.origin').'-viticonnect';
         $f3->set('SESSION.origin', $f3->get('PARAMS.origin'));
@@ -70,107 +62,127 @@ $f3->route('GET /callback/@callback/@origin', function($f3) {
             $sep = '&';
         }
         $f3->reroute($service.$sep.'ticket='.$ticket);
-});
-
-$f3->route('GET /cas/logout', function($f3) {
-    $cases = $f3->get('services');
-    $key = $f3->get('SESSION.origin');
-    $f3->clear('SESSION.origin');
-    $f3->clear('SESSION.ticket');
-    $service = $f3->get('urlbase').'/cas/login';
-    if ($key) {
-        return $f3->reroute(str_replace('%service%', $service, $cases[$key]['cas_logout']));
     }
-    return $f3->reroute($service);
-});
-
-$f3->route('GET /cas/serviceValidate', function($f3) {
-    if (!$f3->exists('GET.service')) {
-        return ;
+    
+    public function cas_logout(Base $f3) {
+        $cases = $f3->get('services');
+        $key = $f3->get('SESSION.origin');
+        $f3->clear('SESSION.origin');
+        $f3->clear('SESSION.ticket');
+        $service = $f3->get('urlbase').'/cas/login';
+        if ($key) {
+            return $f3->reroute(str_replace('%service%', $service, $cases[$key]['cas_logout']));
+        }
+        return $f3->reroute($service);
     }
-    $service = $f3->get('GET.service');
-    if (!$f3->exists('GET.ticket')) {
-        return ;
-    }
-    $cases = $f3->get('services');
-    $full_ticket = $f3->get('GET.ticket');
-    $pos = strpos($full_ticket, '%origin:');
-    $cas_ticket = substr($full_ticket, 0, $pos);
-    $cas_name = str_replace('-viticonnect', '', substr($full_ticket, $pos + 8));
-    $internal_service = $f3->get('urlbase')."/callback/".base64_encode($service)."/".$cas_name;
-    $validator_url = str_replace('%ticket%', $cas_ticket, str_replace('%service%', $internal_service, $cases[$cas_name]['cas_validator']));
-    $raw_xml = file_get_contents($validator_url);
-    if (!$raw_xml) {
-        $f3->set('xml', 'CAS ERROR ('.$validator_url.')');
-       echo View::instance()->render('error.xml.php', 'text/plain');
-       return;
-    }
-    $raw_xml = str_replace('cas:', 'cas_', $raw_xml);
-    if (!strpos($raw_xml, 'cas_authenticationSuccess')) {
-        $f3->set('xml', str_replace($cas_ticket, $full_ticket, $raw_xml));
-        echo View::instance()->render('error.xml.php', 'text/plain');
-        return;
-    }
-    libxml_use_internal_errors(true);
-    $xml = (object) (array) simplexml_load_string($raw_xml);
-    if (libxml_get_errors()){
-        $f3->set('xml', "Error handling xml : ".$raw_xml);
-        echo View::instance()->render('error.xml.php', 'text/plain');
-        return;
-    }
-    if (!isset($xml->cas_authenticationSuccess)) {
-        $f3->set('xml', str_replace($cas_ticket, $full_ticket, $raw_xml));
-        echo View::instance()->render('error.xml.php', 'text/plain');
-        return;
-    }
-    $user_id = $xml->cas_authenticationSuccess->cas_user;
+    
+    public function cas_servicevalidate(Base $f3) {
+        if (!$f3->exists('GET.service')) {
+            return ;
+        }
+        $service = $f3->get('GET.service');
+        if (!$f3->exists('GET.ticket')) {
+            return ;
+        }
+        $cases = $f3->get('services');
+        $full_ticket = $f3->get('GET.ticket');
+        $pos = strpos($full_ticket, '%origin:');
+        $cas_ticket = substr($full_ticket, 0, $pos);
+        $cas_name = str_replace('-viticonnect', '', substr($full_ticket, $pos + 8));
+        $internal_service = $f3->get('urlbase')."/callback/".base64_encode($service)."/".$cas_name;
+        $validator_url = str_replace('%ticket%', $cas_ticket, str_replace('%service%', $internal_service, $cases[$cas_name]['cas_validator']));
+        $raw_xml = file_get_contents($validator_url);
+        if (!$raw_xml) {
+            $f3->set('xml', 'CAS ERROR ('.$validator_url.')');
+            echo View::instance()->render('error.xml.php', 'text/plain');
+            return;
+        }
+        $raw_xml = str_replace('cas:', 'cas_', $raw_xml);
+        if (!strpos($raw_xml, 'cas_authenticationSuccess')) {
+            $f3->set('xml', str_replace($cas_ticket, $full_ticket, $raw_xml));
+            echo View::instance()->render('error.xml.php', 'text/plain');
+            return;
+        }
+        libxml_use_internal_errors(true);
+        $xml = (object) (array) simplexml_load_string($raw_xml);
+        if (libxml_get_errors()){
+            $f3->set('xml', "Error handling xml : ".$raw_xml);
+            echo View::instance()->render('error.xml.php', 'text/plain');
+            return;
+        }
+        if (!isset($xml->cas_authenticationSuccess)) {
+            $f3->set('xml', str_replace($cas_ticket, $full_ticket, $raw_xml));
+            echo View::instance()->render('error.xml.php', 'text/plain');
+            return;
+        }
+        $user_id = $xml->cas_authenticationSuccess->cas_user;
         
-    $api_url = $cases[$cas_name]['api_url'];
-    $secret = $cases[$cas_name]['api_secret'];
-    $epoch = time();
-    $api_url = str_replace('%epoch%', $epoch, $api_url);
-    $api_url = str_replace('%login%', $user_id, $api_url);
-    $api_url = str_replace('%md5%', md5($secret."/".$user_id."/".$epoch), $api_url);
-
-    $raw_api_xml = file_get_contents($api_url);
-    if (!$raw_api_xml) {
-        $f3->set('xml', 'Viticonnect API ERROR ('.$cases[$cas_name]['api_url'].')');
-       echo View::instance()->render('error.xml.php', 'text/plain');
-       return;
-    }
-    $raw_api_xml = str_replace('cas:', 'cas_', $raw_api_xml);
-
-    libxml_use_internal_errors(true);
-    $api_xml = (object) (array) simplexml_load_string($raw_api_xml);
-    if (libxml_get_errors()){
-        $f3->set('xml', "Error handling xml : ".$raw_api_xml);
-        echo View::instance()->render('error.xml.php', 'text/plain');
-        return;
-    }
-
-    $xml->cas_authenticationSuccess = (object)(array) $xml->cas_authenticationSuccess;
-    $xml->cas_authenticationSuccess->cas_attributes = (object)(array) $xml->cas_authenticationSuccess->cas_attributes;
-    if (isset($api_xml->cas_viticonnect_entities_number)) {
-        foreach( (array) $api_xml as $k => $v) {
-            $xml->cas_authenticationSuccess->cas_attributes->{$k} = $v;
+        $api_url = $cases[$cas_name]['api_url'];
+        $secret = $cases[$cas_name]['api_secret'];
+        $epoch = time();
+        $api_url = str_replace('%epoch%', $epoch, $api_url);
+        $api_url = str_replace('%login%', $user_id, $api_url);
+        $api_url = str_replace('%md5%', md5($secret."/".$user_id."/".$epoch), $api_url);
+        
+        $raw_api_xml = file_get_contents($api_url);
+        if (!$raw_api_xml) {
+            $f3->set('xml', 'Viticonnect API ERROR ('.$cases[$cas_name]['api_url'].')');
+            echo View::instance()->render('error.xml.php', 'text/plain');
+            return;
         }
-    }else{
-        $xml->cas_authenticationSuccess->cas_attributes->cas_entities_number = 0;
-    }
-    $f3->set('xml', $xml);
-    echo View::instance()->render('validate.xml.php', 'text/xml');
-});
-
-$f3->route('GET /test', function($f3) {
-    if (!$f3->exists('GET.ticket')) {
-        $auto = '';
-        if ($f3->exists('GET.auto')) {
-            $auto = '&auto='.$f3->get('GET.auto');
+        $raw_api_xml = str_replace('cas:', 'cas_', $raw_api_xml);
+        
+        libxml_use_internal_errors(true);
+        $api_xml = (object) (array) simplexml_load_string($raw_api_xml);
+        if (libxml_get_errors()){
+            $f3->set('xml', "Error handling xml : ".$raw_api_xml);
+            echo View::instance()->render('error.xml.php', 'text/plain');
+            return;
         }
-        return $f3->reroute('/cas/login?service='.$f3->get('urlbase').'/test'.$auto);
+        
+        $xml->cas_authenticationSuccess = (object)(array) $xml->cas_authenticationSuccess;
+        $xml->cas_authenticationSuccess->cas_attributes = (object)(array) $xml->cas_authenticationSuccess->cas_attributes;
+        if (isset($api_xml->cas_viticonnect_entities_number)) {
+            foreach( (array) $api_xml as $k => $v) {
+                $xml->cas_authenticationSuccess->cas_attributes->{$k} = $v;
+            }
+        }else{
+            $xml->cas_authenticationSuccess->cas_attributes->cas_entities_number = 0;
+        }
+        $f3->set('xml', $xml);
+        echo View::instance()->render('validate.xml.php', 'text/xml');
     }
-    $f3->mock('GET /cas/serviceValidate?ticket='.$f3->get('GET.ticket').'&service='.$f3->get('urlbase').'/test');
-});
+    
+    public function test(Base $f3) {
+        if (!$f3->exists('GET.ticket')) {
+            $auto = '';
+            if ($f3->exists('GET.auto')) {
+                $auto = '&auto='.$f3->get('GET.auto');
+            }
+            return $f3->reroute('/cas/login?service='.$f3->get('urlbase').'/test'.$auto);
+        }
+        $f3->mock('GET /cas/serviceValidate?ticket='.$f3->get('GET.ticket').'&service='.$f3->get('urlbase').'/test');
+    }
+    
+}
 
+if(getenv("DEBUG")) {
+    $f3->set('DEBUG', getenv("DEBUG"));
+}
+$f3->set('ROOT', __DIR__);
+$f3->set('UI', $f3->get('ROOT')."/templates/");
+
+$port = $f3->get('PORT');
+$f3->set('urlbase', $f3->get('SCHEME').'://'.$_SERVER['SERVER_NAME'].(!in_array($port,[80,443])?(':'.$port):'').$f3->get('BASE'));
+
+require_once('config/services.config.php');
+$f3->set('services', $services);
+$f3->route('GET /', 'VitiConnect->home');
+$f3->route('GET /cas/login', 'VitiConnect->cas_login_get');
+$f3->route('POST /cas/login', 'VitiConnect->cas_login_post');
+$f3->route('GET /callback/@callback/@origin', 'VitiConnect->callback');
+$f3->route('GET /cas/logout',  'VitiConnect->cas_logout');
+$f3->route('GET /cas/serviceValidate', 'VitiConnect->cas_servicevalidate');
+$f3->route('GET /test', 'VitiConnect->test');
 
 return $f3;
---
+++ config/services.config.php
@@ -0,0 +1,36 @@
+<?php 
+$services = array(
+    'declarvins' => array(
+        "service_humanname" => "Declarvins",
+        "cas_login" => "https://login.declarvins.net/cas/login?service=%service%",
+        "cas_logout" => "https://login.declarvins.net/cas/logout?service=%service%",
+        "cas_validator" => "https://login.declarvins.net/cas/serviceValidate?service=%service%&ticket=%ticket%",
+        "api_url" => "https://declaration.declarvins.net/viticonnect/api/%login%/%epoch%/%md5%",
+    ),
+    "odgrhone" => array(
+        "service_humanname" => "Syndicat des Côtes du Rhône",
+        "cas_login" => "https://login.syndicat-cotesdurhone.com/cas/login?service=%service%",
+        "cas_logout" => "https://login.syndicat-cotesdurhone.com/cas/logout?service=%service%",
+        "cas_validator" => "https://login.syndicat-cotesdurhone.com/cas/serviceValidate?service=%service%&ticket=%ticket%",
+        "api_url" => "https://declaration.syndicat-cotesdurhone.com/viticonnect/api/%login%/%epoch%/%md5%",
+    ),
+    "igpvaucluse" => array(
+        "service_humanname" => "Vins IGP Vaucluse",
+        "cas_login" => "https://login.igp.vins.24eme.fr/cas_igp/login?instance=vaucluse&service=%service%",
+        "cas_logout" => "https://login.igp.vins.24eme.fr/cas_igp/logout?instance=vaucluse&service=%service%",
+        "cas_validator" => "https://login.igp.vins.24eme.fr/cas_igp/serviceValidate?service=%service%&ticket=%ticket%",
+        "api_url" => "https://vaucluse.igp.vins.24eme.fr/viticonnect/api/%login%/%epoch%/%md5%",
+    ),
+    "odgprovence" => array(
+        "service_humanname" => "Syndicat des Côtes de Provence",
+        "cas_login" => "https://login.syndicat-cotesdeprovence.com/cas/login?instance=vaucluse&service=%service%",
+        "cas_logout" => "https://login.syndicat-cotesdeprovence.com/cas/logout?instance=vaucluse&service=%service%",
+        "cas_validator" => "https://login.syndicat-cotesdeprovence.com/cas/serviceValidate?service=%service%&ticket=%ticket%",
+        "api_url" => "https://declaration.syndicat-cotesdeprovence.com/viticonnect/api/%login%/%epoch%/%md5%",
+    )
+);
+
+
+if (file_exists(__DIR__."/apisecrets.config.php")) {
+    include(__DIR__."/apisecrets.config.php");
+}
